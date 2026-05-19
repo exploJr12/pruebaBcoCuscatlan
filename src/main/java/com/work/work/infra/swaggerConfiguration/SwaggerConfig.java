e3c71371a22c7626780a402e7db9643ab8d38029
Daniel Cortez <105823496+exploJr12@users.noreply.github.com>
2026-05-19 09:29:28 -0600

    databases config

--- src/main/java/com/work/work/infra/swaggerConfiguration/SwaggerConfig.java
+++ src/main/java/com/work/work/infra/swaggerConfiguration/SwaggerConfig.java
@@ -3,32 +3,26 @@ package com.work.work.infra.swaggerConfiguration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "Carrito de Compras",
-                description = "Api de carrito de compras v1.0.0 con clientes y producto con opcion de modificar eliminar y crear.",
-                version = "v1.0.0",
+                title = "Shopping Cart API",
+                description = "API para prueba técnica de carrito de compras con autenticación JWT",
+                version = "1.0.0",
                 contact = @Contact(
-                        name = "Edwin Castro",
-                        email = "Castromaradiaga@hotmail.com",
-                        url = "www.EDTecnology.com"
-                ),
-                license = @License(
-                        name = "libre de uso",
-                        url = "www.EDTecnology.com"
+                        name = "Daniel Cortez",
+                        email = "daniel@email.com"
                 )
         ),
         servers = {
                 @Server(
                         url = "http://localhost:8080",
-                        description = "SERVER DE DESARROLLO"
+                        description = "Servidor local"
                 )
         }
-
 )
-
 public class SwaggerConfig {
 }
